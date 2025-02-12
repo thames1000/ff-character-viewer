3c646bea15457f015843310784dcb9ecf8fa4901
James Lambert <james.t.lambert26.mil@army.mil>
2025-02-12 12:06:47 -0500

    Putting original back

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,10 @@
 const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const CopyPlugin = require('copy-webpack-plugin');
 
 module.exports = {
-    mode: process.env.NODE_ENV === 'production' ? 'production' : 'development',
-    entry: './src/app.js',
+    entry: './src/index.js',
     output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: '[name].[contenthash].js',
-        clean: true
+        path: path.resolve(__dirname, 'public/dist'),
+        filename: 'bundle.js',
     },
     module: {
         rules: [
@@ -16,37 +12,6 @@ module.exports = {
                 test: /\.css$/,
                 use: ['style-loader', 'css-loader'],
             },
-            {
-                test: /\.js$/,
-                exclude: /node_modules/,
-                use: {
-                    loader: 'babel-loader',
-                    options: {
-                        presets: ['@babel/preset-env']
-                    }
-                }
-            }
         ],
     },
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: './index.html'
-        }),
-        new CopyPlugin({
-            patterns: [
-                { from: "styles.css", to: "styles.css" }
-            ],
-        }),
-    ],
-    resolve: {
-        extensions: ['.js']
-    },
-    devServer: {
-        static: {
-            directory: path.join(__dirname, 'dist'),
-        },
-        compress: true,
-        port: 3000,
-        open: true
-    }
-};
+}; 
